87280a8f78f4535e0ded22478f8b4fa846243336
ArkadiuszBorkowski <borkowskiarkadiusz91@gmail.com>
2023-10-05 23:20:51 +0200

    Wykorzystnie konstruktoorów do tworzenia zbiorów książek

--- src/Book.java
+++ src/Book.java
@@ -5,4 +5,20 @@ class Book {
     int pages;
     String publisher;
     String isbn;
+
+    Book(String bookTitle, String bookAuthor, int bookRelease,
+         int bookPages, String bookPublisher, String bookIsbn) {
+        title = bookTitle;
+        author = bookAuthor;
+        releaseDate = bookRelease;
+        pages = bookPages;
+        publisher = bookPublisher;
+        isbn = bookIsbn;
+    }
+
+    void printInfo() {
+        String info = title + "; " + author + "; " + releaseDate + "; "
+                + pages + "; " + publisher + "; " + isbn;
+        System.out.println(info);
+    }
 }
--- src/Library.java
+++ src/Library.java
@@ -1,23 +1,17 @@
 class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.2";
+        final String appName = "Biblioteka v0.3";
 
-        Book book1 = new Book();
+        Book book1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010, 296, "Greg", "9788373271890");
+        Book book2 = new Book("Java. Efektywne programowanie. Wydanie II", "Joshua Bloch", 2009, 352, "Helion",
+                "9788324620845");
+        Book book3 = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide", "Bert Bates, Katherine Sierra",
+                2008, 851, "McGraw-Hill Osborne Media", "9780071591065");
 
-        book1.title = "W pustyni i w puszczy";
-        book1.author = "Henryk Sienkiewicz";
-        book1.releaseDate = 2010;
-        book1.pages = 296;
-        book1.publisher = "Greg";
-        book1.isbn = "9788373271890a";
-
-        System.out.println(appName);
+        System.out.print(appName);
         System.out.println("Książki dostępne w bibliotece:");
-        System.out.println(book1.title);
-        System.out.println(book1.author);
-        System.out.println(book1.releaseDate);
-        System.out.println(book1.pages);
-        System.out.println(book1.publisher);
-        System.out.println(book1.isbn);
+        book1.printInfo();
+        book2.printInfo();
+        book3.printInfo();
     }
 }
