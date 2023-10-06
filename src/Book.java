5337ce58f5117250f8cd58ce84d97ea59c9965eb
ArkadiuszBorkowski <borkowskiarkadiusz91@gmail.com>
2023-10-06 12:37:27 +0200

    Dodanie przeciążonych konstruktoorów, wykorzystanie this

--- src/Book.java
+++ src/Book.java
@@ -6,14 +6,17 @@ class Book {
     String publisher;
     String isbn;
 
-    Book(String bookTitle, String bookAuthor, int bookRelease,
-         int bookPages, String bookPublisher, String bookIsbn) {
-        title = bookTitle;
-        author = bookAuthor;
-        releaseDate = bookRelease;
-        pages = bookPages;
-        publisher = bookPublisher;
-        isbn = bookIsbn;
+    public Book(String title, String author, int releaseDate, int pages, String publisher) {
+        this.title = title;
+        this.author = author;
+        this.releaseDate = releaseDate;
+        this.pages = pages;
+        this.publisher = publisher;
+    }
+
+    public Book(String title, String author, int releaseDate, int pages, String publisher, String isbn) {
+        this(title, author, releaseDate, pages, publisher);
+        this.isbn = isbn;
     }
 
     void printInfo() {
