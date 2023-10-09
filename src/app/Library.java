7781b4189fdfd4a6860c00816353bb157eb292c2
ArkadiuszBorkowski <borkowskiarkadiusz91@gmail.com>
2023-10-09 23:03:56 +0200

    Wykorzystanie tablic do przechowywania ksobiektów - klasa Book

--- src/app/Library.java
+++ src/app/Library.java
@@ -4,18 +4,22 @@ import model.Book;
 
 class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.4";
+        final String appName = "Biblioteka v0.7";
 
-        Book book1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010, 296, "Greg", "9788373271890");
-        Book book2 = new Book("Java. Efektywne programowanie. Wydanie II", "Joshua Bloch", 2009, 352, "Helion",
-                "9788324620845");
-        Book book3 = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide", "Bert Bates, Katherine Sierra",
-                2008, 851, "McGraw-Hill Osborne Media");
+        Book[] books = new Book[1000];
+
+        books[0] = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010, 296, "Greg",
+                "9788373271890");
+        books[1] = new Book("Java. Efektywne programowanie. Wydanie II", " Joshua Bloch", 2009,
+                352, "Helion", "9788324620845");
+        books[2] = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide",
+                "Bert Bates, Katherine Sierra", 2008, 851, "McGraw-Hill Osborne Media");
 
         System.out.println(appName);
         System.out.println("Książki dostępne w bibliotece:");
-        book1.printInfo();
-        book2.printInfo();
-        book3.printInfo();
+        books[0].printInfo();
+        books[1].printInfo();
+        books[2].printInfo();
+        System.out.println("System może przechowywać do " + books.length + " książek");
     }
 }
