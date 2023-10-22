7acf9d309b0f42d2ac0f387ff910f1ae14275608
ArkadiuszBorkowski <borkowskiarkadiusz91@gmail.com>
2023-10-22 22:34:14 +0200

    dodanie typów Enum zamiast stałych

--- src/app/LibraryControl.java
+++ src/app/LibraryControl.java
@@ -6,11 +6,6 @@ import model.Library;
 import model.Magazine;
 
 public class LibraryControl {
-    private static final int EXIT = 0;
-    private static final int ADD_BOOK = 1;
-    private static final int ADD_MAGAZINE = 2;
-    private static final int PRINT_BOOKS = 3;
-    private static final int PRINT_MAGAZINES = 4;
 
     // zmienna do komunikacji z użytkownikiem
     private DataReader dataReader = new DataReader();
@@ -22,11 +17,11 @@ public class LibraryControl {
      * Główna metoda programu, która pozwala na wybór opcji i interakcję
      */
     public void controlLoop() {
-        int option;
+        Option option;
 
         do {
             printOptions();
-            option = dataReader.getInt();
+            option = Option.createFromInt(dataReader.getInt());
             switch (option) {
                 case ADD_BOOK:
                     addBook();
@@ -46,7 +41,7 @@ public class LibraryControl {
                 default:
                     System.out.println("Nie ma takiej opcji, wprowadź ponownie: ");
             }
-        } while (option != EXIT);
+        } while (option != Option.EXIT);
     }
 
     private void printMagazines() {
@@ -60,11 +55,9 @@ public class LibraryControl {
 
     private void printOptions() {
         System.out.println("Wybierz opcję: ");
-        System.out.println(EXIT + " - wyjście z programu");
-        System.out.println(ADD_BOOK + " - dodanie nowej książki");
-        System.out.println(ADD_MAGAZINE + " - dodanie nowego magazynu");
-        System.out.println(PRINT_BOOKS + " - wyświetl dostępne książki");
-        System.out.println(PRINT_MAGAZINES + " - wyświetl dostępne magazyny");
+        for (Option value : Option.values()) {
+            System.out.println(value);
+        }
     }
 
     private void addBook() {
---
+++ src/app/Option.java
@@ -0,0 +1,36 @@
+package app;
+
+public enum Option {
+    EXIT(0,"wyjście z programu"),
+    ADD_BOOK(1, "dodanie nowej książki"),
+    ADD_MAGAZINE(2, "dodanie nowego magazynu"),
+    PRINT_BOOKS(3, "wyświetl dostępne książki"),
+    PRINT_MAGAZINES(4, "wyświetl dostępne magazyny");
+
+    private final int value;
+    private final String description;
+
+    Option(int value, String description) {
+        this.value = value;
+        this.description = description;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return value + " - " + description;
+    }
+
+    static Option createFromInt(int option) {
+        return Option.values()[option];
+    }
+}
+
+
